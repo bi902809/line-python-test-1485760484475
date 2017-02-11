bbe97a7a141167d0b10234a31a1ce0cd94705ad3
S Abe <BI902809@jp.ibm.com>
2017-02-11 12:38:05 +0900

    Fix

--- server.py
+++ server.py
@@ -124,10 +124,11 @@ def firstAction(event, output):
 	text = ''
 	print(output)
 	if 'text' in output:
-		text = str(output['text'])
+		for x in output['text']
+		text = text + '\n' + x
 	line_bot_api.reply_message(
 		event.reply_token,
-		TextSendMessage(text=text)
+		TextSendMessage(text=text[1:])
 	)
 
 
